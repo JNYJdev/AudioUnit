fc8b02111f81c5cacf6b84ee3862172c7ffadca2
William.zhou <JNYJdev@users.noreply.github.com>
2016-12-14 14:36:29 +0800

    Update file audio unit-- code from google codes

---
+++ IosAudioController.h
@@ -0,0 +1,27 @@
+//
+//  IosAudioController.h
+//  Aruts
+//
+//  Created by Simon Epskamp on 10/11/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <AudioToolbox/AudioToolbox.h>
+
+@interface IosAudioController : NSObject {
+	AudioComponentInstance audioUnit;
+	AudioBuffer tempBuffer; // this will hold the latest data from the microphone
+}
+
+@property (readonly) AudioComponentInstance audioUnit;
+@property (readonly) AudioBuffer tempBuffer;
+
+- (void) start;
+- (void) stop;
+- (void) processAudio: (AudioBufferList*) bufferList;
+
+@end
+
+// setup a global iosAudio variable, accessible everywhere
+extern IosAudioController* iosAudio;
